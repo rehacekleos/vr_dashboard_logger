90596f398ef15821f39bdc661bf203e37e69d0e1
leosrehacek <leos.rehacek@epptec.eu>
2023-11-15 22:17:42 +0100

    update

--- Editor/Classes/VrData.cs
+++ Editor/Classes/VrData.cs
@@ -19,6 +19,10 @@ namespace Editor.Classes
         public List<Record> records;
         [CanBeNull] public string custom_data;
 
+        public VrData()
+        {
+        }
+
         public VrData(string applicationIdentifier, string logVersion, int logRate, [CanBeNull] string customData)
         {
             application_identifier = applicationIdentifier;
